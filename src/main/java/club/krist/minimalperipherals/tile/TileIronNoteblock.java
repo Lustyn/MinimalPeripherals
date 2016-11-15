a7b89c1bfd1f0adf72c8d6258b9192f004a7cf65
Justyn <justync9@gmail.com>
2016-11-15 17:49:56 -0600

    update to 1.3

--- src/main/java/club/krist/minimalperipherals/tile/TileIronNoteblock.java
+++ src/main/java/club/krist/minimalperipherals/tile/TileIronNoteblock.java
@@ -3,9 +3,14 @@ package club.krist.minimalperipherals.tile;
 import club.krist.minimalperipherals.MinimalPeripherals;
 import club.krist.minimalperipherals.util.LuaMethod;
 import dan200.computercraft.api.lua.LuaException;
+import net.minecraft.network.play.server.SPacketCustomSound;
+import net.minecraft.network.play.server.SPacketSoundEffect;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
 /**
@@ -16,19 +21,23 @@ public class TileIronNoteblock extends TilePeripheral {
 
     private final String[] instruments = {"harp", "basedrum", "snare", "hat", "bass"};
 
+    private void playSound(BlockPos pos, String soundIn, SoundCategory category, float volume, float pitch) {
+        FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().sendToAllNearExcept(null, pos.getX(), pos.getY(), pos.getZ(), volume > 1.0F ? (double)(16.0F * volume) : 16.0D, getWorld().provider.getDimension(), new SPacketCustomSound(soundIn, category, pos.getX(), pos.getY(), pos.getZ(), volume, pitch));
+    }
+
     @LuaMethod
     public void playNote(Object[] arguments) throws LuaException {
         if (arguments.length < 2)
             throw new LuaException("Wrong number of arguments. 2 expected.");
-        if (!(arguments[0] instanceof Double) && ((Double) arguments[0] >= 0.0D && (Double) arguments[0] < 5.0D))
+        if (!(arguments[0] instanceof Double) | (int) (double) (Double) arguments[0] < 0 | (int) (double) (Double) arguments[0] > 4)
             throw new LuaException("Bad argument #1 (expected number 0-4)");
         if (!(arguments[1] instanceof Double))
             throw new LuaException("Bad argument #2 (expected number)");
 
-        String instrument = "block.note." + instruments[(int) (double) (Double) arguments[0]];
+        String instrument = "minecraft:block.note." + instruments[(int) (double) (Double) arguments[0]];
         float note = (float) Math.pow(2D, ((int) (double) (Double) arguments[1] - 12) / 12D);
-
-        getWorld().playSound(null, getPos().getX() + 0.5D, getPos().getY() + 0.5D, getPos().getZ() + 0.5D, new SoundEvent(new ResourceLocation(instrument)), SoundCategory.RECORDS, 3F, note);
+        //getWorld().playSound(null, getPos().getX() + 0.5D, getPos().getY() + 0.5D, getPos().getZ() + 0.5D, new SoundEvent(new ResourceLocation(instrument)), SoundCategory.RECORDS, 3F, note);
+        playSound(getPos(), instrument, SoundCategory.RECORDS, 1F, note);
     }
 
     @LuaMethod
@@ -45,8 +54,7 @@ public class TileIronNoteblock extends TilePeripheral {
         String sound = (String) arguments[0];
         float volume = (float) (double) (Double) arguments[1];
         float pitch = (float) (double) (Double) arguments[2];
-
-        getWorld().playSound(null, getPos().getX() + 0.5D, getPos().getY() + 0.5D, getPos().getZ() + 0.5D, new SoundEvent(new ResourceLocation(sound)), SoundCategory.RECORDS, volume, pitch);
+        playSound(getPos(), sound, SoundCategory.RECORDS, volume, pitch);
     }
 
     public String getType() {
